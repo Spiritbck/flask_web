9aafc3470eb4021921dd7d09c384ea5c50f4d81e
Clinton <61786912+Spiritbck@users.noreply.github.com>
2020-08-27 20:12:24 -0700

    Update py3webserver.py

--- py3webserver.py
+++ py3webserver.py
@@ -50,7 +50,8 @@ class webserverHandler(BaseHTTPRequestHandler ):
             output += "<html><body>"
             output += " <h2> Okay, how about this: </h2>"
             output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?
+                      </h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
             output += "</body></html>"
             self.wfile.write(output)
             print (output)
